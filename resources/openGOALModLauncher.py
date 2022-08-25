06b2359bd90443811e88256960b506c7621e1195
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2022-08-25 12:04:08 -0700

    try/except around decode, if it throws exception then probably it was the generic task not found message with non-utf8 chars

--- resources/openGOALModLauncher.py
+++ resources/openGOALModLauncher.py
@@ -63,12 +63,15 @@ def show_progress(block_num, block_size, total_size):
 
 def process_exists(process_name):
     call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
-    # use buildin check_output right away
-    output = subprocess.check_output(call).decode()
-    # check in last line for process name
-    last_line = output.strip().split('\r\n')[-1]
-    # because Fail message could be translated
-    return last_line.lower().startswith(process_name.lower())
+    try:
+        # use buildin check_output right away
+        output = subprocess.check_output(call).decode()
+        # check in last line for process name
+        last_line = output.strip().split('\r\n')[-1]
+        # because Fail message could be translated
+        return last_line.lower().startswith(process_name.lower())
+    except:
+        return False
 
 def try_kill_process(process_name):
 	if process_exists(process_name):
